45d12c3158f180a9c12f418680f42e4e4e67b742
JoMingyu <city7310@naver.com>
2018-04-12 13:31:16 +0900

    UPDATE: href_to_markdown_link_format

--- href_to_markdown_link_format.py
+++ href_to_markdown_link_format.py
@@ -1,11 +1,15 @@
+import re
+
 f = open('README.md')
 data = f.readlines()
 
+link_regex = re.compile('".+"')
+content_regex = re.compile('>.+<')
+
 for idx, line in enumerate(data):
     if 'a href' in line:
-        import re
-        link = re.findall('".+"', line)[0][1:-1]
-        content = re.findall('>.+<', line)[0][1:-1]
+        link = link_regex.findall(line)[0][1:-1]
+        content = content_regex.findall(line)[0][1:-1]
         new_line = '- [{}]({})\n'.format(content, link)
         data[idx] = new_line
 
